ba7af225096587b6ea8cbbb68c95e5c4907d1ab5
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-09 22:17:24 -0300

    Add files via upload

---
+++ Sessão 3/Exercícios Propostos - Parte 02/Exercício 01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+
+            Console.Write("Digite um número inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num < 0) {
+
+                Console.WriteLine("\nO número é negativo!");
+
+            } else if (num > 0)
+            {
+                Console.WriteLine("\nO número é positivo!");
+            } else
+            {
+                Console.WriteLine("\nO número é nulo!");
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Sessão 3/Exercícios Propostos - Parte 02/Exercício 03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao 
+            //Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em 
+            //ordem crescente ou decrescente.
+
+            Console.Write("Digite dois números inteiros separados por espaço para verificar se são multiplos: ");
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int a = int.Parse(valores[0]);
+            int b = int.Parse(valores[1]);
+
+            if (a % b == 0 || b % a == 0) {
+
+                Console.WriteLine($"\nOS números {a} e {b} são multiplos entre si!");
+
+            } else
+            {
+                Console.WriteLine($"\nOs números {a} e {b} não são multiplos!");
+            }
+
+        }
+    }
+}
