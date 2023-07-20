7833e38b001d1bac07883304689116ff342cae2e
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-19 21:45:54 -0300

    Add files via upload

---
+++ Sessão 3/32. While/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _32.While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0, raiz = 0;
+
+
+            Console.Write("Digite um número: ");
+            x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x >= 0.0)
+            {
+                raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Digite outro número: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("\nNúmero Negativo, Zé!");
+            
+
+            
+
+
+        }
+    }
+}
