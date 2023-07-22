6f98f78f0e008aa196d55ee7d56b7faa129e1386
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-22 16:50:46 -0300

    Add files via upload

---
+++ Sessão 3/Exercícios Propostos - Parte 04/Exercício 01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Leia um valor inteiro X(1 <= X <= 1000).Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+            //X, se for o caso.
+
+            int num = 0, i = 0;
+
+            Console.Write("Digite um valor de 1 a 1000: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num < 1 || num > 1000)
+            {
+                Console.WriteLine("\nDigite um número válido!");
+
+            } 
+            else
+            {
+                for (i = 1; i <= num; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
