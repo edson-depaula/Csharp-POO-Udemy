a5cf43532ef2a1316c2ce25c3b501c69878e540f
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-23 12:20:40 -0300

    Add files via upload

---
+++ Seção 03/Exercícios Propostos - Parte 04/Exercício 05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+            Lembrando que, por definição, fatorial de 0 é 1.
+            */
+
+            int fat = 1;
+
+            Console.Write("Digite um valor inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= numero; i++)
+            {
+                fat *= i;
+            }
+
+            Console.WriteLine($"\nO fatorial de {numero} é {fat}");
+
+
+
+        }
+    }
+}
---
+++ Seção 03/Exercícios Propostos - Parte 04/Exercício 06/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            //Ler um número inteiro N e calcular todos os seus divisores
+            Entrada:
+            6
+            Saída:
+            1
+            2
+            3
+            6
+            */
+
+            int num = 0;
+            Console.Write("Digite um número inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nOs divisores de {num} são:");
+            for(int i = 1; num >= i; i++)
+            {
+                if(num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
