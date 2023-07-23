8efda8471e9b5b7bddea278f660aa1a00493fe2e
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-23 12:16:44 -0300

    Add files via upload

---
+++ Seção 03/Exercícios Propostos - Parte 02/Exercício 02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar
+
+            int num = 0;
+
+            Console.Write("Digite um número inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0) {
+
+                Console.WriteLine($"\nO número {num} é par!");
+
+            } else
+            {
+                Console.WriteLine($"\nO número {num} é impar!");
+            }
+        }
+    }
+}
---
+++ Seção 03/Exercícios Propostos - Parte 02/Exercício 04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode 
+            //começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas
+
+            int horaIni = 0, horaFin = 0, duracao = 0; ;
+
+            Console.Write("A que horas o jogo iniciou? ");
+            horaIni = int.Parse(Console.ReadLine());
+
+            Console.Write("A que horas o jogo acabou? ");
+            horaFin = int.Parse(Console.ReadLine());
+
+            if (horaIni < horaFin)
+            {
+                duracao = horaFin - horaIni;
+            }
+            else
+            {
+                duracao = 24 - horaIni + horaFin;
+            }
+
+            Console.WriteLine($"\nA duração do jogo foi de {duracao} horas.");
+
+        }
+    }
+}
