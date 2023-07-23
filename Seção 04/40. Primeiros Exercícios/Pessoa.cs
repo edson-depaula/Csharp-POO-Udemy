15b9393ceb4bd226caddc2d5351f09de1aad3b14
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-23 14:21:42 -0300

    Add files via upload

---
+++ Seção 04/40. Primeiros Exercícios/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40.Primeiros_Exercícios
+{
+    internal class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ Seção 04/40. Primeiros Exercícios/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40.Primeiros_Exercícios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Digite os dados da primeira pessoa, nome e idade: ");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDigite os dados da segunda pessoa, nome e idade: ");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if(p1.Idade > p2.Idade)
+            {
+                Console.WriteLine($"\nPessoa mais velha: {p1.Nome}\n");
+            }
+            else
+            {
+                Console.WriteLine($"\nPessoa mais velha: {p2.Nome}\n");
+            }
+
+
+
+
+        }
+    }
+}
