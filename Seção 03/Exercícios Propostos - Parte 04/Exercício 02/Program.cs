fb619f1156fb355667549f9fdc831a743f411f2f
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-23 12:19:54 -0300

    Add files via upload

---
+++ Seção 03/Exercícios Propostos - Parte 04/Exercício 02/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+            Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando 
+            essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).
+            */
+
+            int n = 0, i = 0, x = 0, cont_in = 0, cont_out = 0;
+
+            Console.Write("Digite uma quantidade de números: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write($"Digite o {i}º número:");
+                x = int.Parse(Console.ReadLine());
+
+                if (x >= 10 && x <= 20)
+                {
+                    cont_in += 1;
+                }
+                else
+                {
+                    cont_out += 1;
+                }
+            }
+
+            Console.WriteLine($"\nHá {cont_in} números dentro do intervalo de [10,20]");
+            Console.WriteLine($"Há {cont_out} números fora do intervalo de [10,20]\n");
+
+
+        }
+    }
+}
---
+++ Seção 03/Exercícios Propostos - Parte 04/Exercício 03/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercício_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste 
+            de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes 
+            conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem 
+            peso 5.
+            Entrada: 
+            3
+            6.5 4.3 6.2
+            5.1 4.2 8.1
+            8.0 9.0 10.0
+            Saída:
+            5.7
+            6.3
+            9.3
+            */
+
+            Console.Write("Digite a quantidade de testes: ");
+            int qtd = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            for(int i = 1; i <= qtd; i++)
+            {
+                Console.Write("Digite três notas separadas por espaço: ");
+                string[] notas = Console.ReadLine().Split(' ');
+                double a = double.Parse(notas[0], CultureInfo.InvariantCulture);
+                double b = double.Parse(notas[1], CultureInfo.InvariantCulture);
+                double c = double.Parse(notas[2], CultureInfo.InvariantCulture);
+
+                double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+
+                Console.WriteLine("A média ponderada é de: " + media.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("");
+            }
+
+
+
+
+
+        }
+    }
+}
