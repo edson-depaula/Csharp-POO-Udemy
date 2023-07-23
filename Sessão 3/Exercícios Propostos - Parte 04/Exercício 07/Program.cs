d8019989dced6ed734e2215e034d42adcd6546a6
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-22 22:47:09 -0300

    Add files via upload

---
+++ Sessão 3/Exercícios Propostos - Parte 04/Exercício 07/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas, 
+            começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+            exemplo
+            */
+
+            Console.Write("Digite um número positivo: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n <= 0)
+            {
+                while (n <= 0)
+                {
+                    Console.Write("Digite um número positivo (maior que zero): ");
+                    n = int.Parse(Console.ReadLine());
+                }
+            }
+            if (n > 0)
+            {
+                Console.WriteLine("");
+                for(int i = 1; i <= n; i++)
+                {
+                    Console.WriteLine($"{i}, ao quadrado {Math.Pow(i, 2)}, ao cubo {Math.Pow(i, 3)}");
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
