1df868329b6dce7ae891d591c2ac9b5a02259d23
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-23 12:17:46 -0300

    Add files via upload

---
+++ Seção 03/Exercícios Propostos - Parte 02/Exercício 05/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercício_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A 
+            //seguir, calcule e mostre o valor da conta a pagar.
+
+
+            int cod = 0, qtd = 0;
+
+            Console.Write("Digite o código do item desejado: ");
+            cod = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade do item desejado: ");
+            qtd = int.Parse(Console.ReadLine());
+
+            double total;
+            if (cod == 1)
+            {
+                total = qtd * 4.0;
+            }
+            else if (cod == 2)
+            {
+                total = qtd * 4.5;
+            }
+            else if (cod == 3)
+            {
+                total = qtd * 5.0;
+            }
+            else if (cod == 4)
+            {
+                total = qtd * 2.0;
+            }
+            else
+            {
+                total = qtd * 1.5;
+            }
+
+            Console.WriteLine("\nTotal a pagar: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
---
+++ Seção 03/Exercícios Propostos - Parte 02/Exercício 08/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercício_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e 
+            mostre o valor que esta pessoa deve pagar de aliquota de Renda, segundo a tabela abaixo.
+            Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de 
+            salário que fica de R$ 0.00 até R$ 2000.00 é isenta de aliquota de Renda. No exemplo fornecido (abaixo), a taxa é 
+            de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total. O valor deve ser impresso com 
+            duas casas decimais.
+            */
+
+            double salario = 0, aliquota = 0;
+
+            Console.Write("Digite o valor do seu salário: ");
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (salario <= 2000.0)
+            {
+                aliquota = 0.0;
+            }
+            else if (salario <= 3000.0)
+            {
+                aliquota = (salario - 2000.0) * 0.08;
+            }
+            else if (salario <= 4500.0)
+            {
+                aliquota = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
+            }
+            else
+            {
+                aliquota = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+            }
+
+            if (aliquota == 0.0)
+            {
+                Console.WriteLine("Isento");
+            }
+            else
+            {
+                Console.WriteLine("Pague de imposto R$ " + aliquota.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+
+        }
+    }
+}
