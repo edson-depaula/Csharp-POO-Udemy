c47d82fdb682a5bd606d0686d38d0e9c40bb919e
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-23 15:07:48 -0300

    Add files via upload

---
+++ Seção 04/41. Criando método para reaproveitamento e delegação/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _38.Problema_sem_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarar as variáveis compostas dessa classe triângulo
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            //x.a ou x.b ou x.c acessa o atributo da variável na classe
+            //Assim já armazena as informações no objeto composto "x" da classe triângulo
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("\nEntre com as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Abre e fecha parenteses é obrigatório para indicar método
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine("\nÁrea de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if(areaX > areaY)
+            {
+                Console.WriteLine("\nMaior área: X");
+            }
+            else
+            {
+                Console.WriteLine("\nMaior área: Y");
+            }
+
+            //Os objetos são instâncias das classes | X é a instância | A, B e C são os atributos
+
+            // X   3.0 4.0 5.0
+            //      A   B   C
+        }
+    }
+}
---
+++ Seção 04/41. Criando método para reaproveitamento e delegação/Triangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38.Problema_sem_POO
+{
+    internal class Triangulo
+    {
+        //atributos A, B e C
+        public double A;
+        public double B;
+        public double C;
+
+        //Método é uma função dentro da classe
+        //o "public" permite que o método seja acessado em outro arquivo
+        public double Area()
+        {
+            //A, B e C ele enxerga direto os atributos, e não os triangulos x ou y
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
