e0cd06a60d758230272650f3a17aff9aaa38183a
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-23 12:21:16 -0300

    Add files via upload

---
+++ Seção 03/Fórmula de Bhaskara/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Olá! Hoje irei lhe ajudar com a fórmula de Bhaskara.\n");
+
+double a = 0, b = 0, c = 0, delta = 0, x1 = 0, x2 = 0;
+
+Console.Write("Digite o valor de \"A\": ");
+a = double.Parse(Console.ReadLine());
+Console.Write("Digite o valor de \"B\": ");
+b = double.Parse(Console.ReadLine());
+Console.Write("Digite o valor de \"C\": ");
+c = double.Parse(Console.ReadLine());
+
+delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+if (delta < 0)
+{
+    Console.WriteLine("\nNão existem raízes reais para a equação.");
+}
+else
+{
+    x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+    x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+    Console.WriteLine($"\nO valor de Delta é: {delta}.\n");
+    Console.WriteLine($"O valor de \"X1\" é igual a: {x1}.");
+    Console.WriteLine($"O valor de \"X2\" é igual a: {x2}.");
+}
